a5a26a8c57cc7ab51ae5cb7165cd68b7e65a17d6
Yi QI <jnjnqy@gmail.com>
2021-09-24 00:58:21 +0800

    v0.4.7

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8
 from distutils.core import setup
 setup(
   name = 'Flask-KQMaps',
   packages = ['flask_kqmaps'], 
-  version = '0.4',
+  package_data={'flask_kqmaps': ['templates/*', 'static/*', 'static/*/*', 'static/*/*/*', 'static/*/*/*/*', 'static/*/*/*/*/*', 'static/*/*/*/*/*/*', 'static/*/*/*/*/*/*/*'], },
+  version = '0.4.2',
   license='GNU GPLv3',
   description = 'Flask Extension for KQWebMap API for Leaflet (aka KQClient for Leaflet API)',
   author = 'Yi QI',
@@ -16,8 +18,9 @@ setup(
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
+    'Topic :: Software Development :: Libraries :: Python Modules',
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3',
+    'Framework :: Flask',
   ],
 )
