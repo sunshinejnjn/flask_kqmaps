8badc48571240828e96027528073d1872a26cea6
Yi QI <jnjnqy@gmail.com>
2021-09-23 17:09:31 +0800

    v0.4.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GPLv3',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3',
   ],
 )
